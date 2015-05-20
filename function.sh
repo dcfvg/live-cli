0917b1f0ec02bc8d465e640db22228fd19b19908
dcfvg <qwsx@dcfvg.com>
2015-05-20 14:31:27 +0200

    + init project function

--- function.sh
+++ function.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+# set -x
+
 function cpRsaKey {
   # copy rsa key in .ssh folder in $1
-  cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir .ssh/; cat >> .ssh/authorized_keys'
+  cat ~/.ssh/id_rsa.pub | ssh $1 '[ ! -d .ssh/ ] && mkdir .ssh/; cat >> .ssh/authorized_keys'
 }
 
 function installSoft {
@@ -12,23 +15,38 @@ function installSoft {
   sudo apt-get install  netatalk screen \
                         detox bash-completion watch \
                         sane sane-utils libsane-extras xsane \
-                        streamer ffmpeg \
+                        streamer ffmpeg gifsicle \
                         jpegoptim imagemagick gphoto2 libimage-exiftool-perl
   exit                
   '
 }
 
 function initProject {
-
+  # init project and clone git repository
 
   ssh $1 '
 
+  cli="Scripts/custom/live-cli"
+  content="$cli/content/"
+
+  [ ! -d $cli ]     && mkdir -vp $cli; git clone https://github.com/dcfvg/live-cli.git $cli
+  [ ! -d $content ] && mkdir -vp $content
 
   exit 
   '
-
-
-
-
-
 }
+
+function updateProjet {
+  # get code last version
+
+  ssh $1 '
+  
+  cli="Scripts/custom/live-cli"
+  content="$cli/content/"
+
+  cd $cli
+  git pull
+  exit 
+  '
+}
+
