0a908a4b7993249b625b718a63816c6ce62de22a
dcfvg <qwsx@dcfvg.com>
2015-05-20 14:58:43 +0200

    fix get image after capture

--- daemon.sh
+++ daemon.sh
@@ -8,8 +8,8 @@ source config.sh
 size=350
 target="/Users/benoit/Scripts/custom/live-cli/content/anim-e"
 
-rsync -avz --remove-source-files -e ssh $rpie:~/Scripts/*.jpeg $target 
 ssh $rpie 'streamer -f jpeg -s 1920x1080 -o Scripts/$(date +"%y.%m.%d-%H.%M.%S").jpeg; exit'
+rsync -avz --remove-source-files -e ssh $rpie:~/Scripts/*.jpeg $target 
 
 convert -resize $size \
 				-loop 0  -delay 1x25 \
