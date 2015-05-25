e9fb33a06ad40583a5ab60d277da49f86fef263c
dcfvg <qwsx@dcfvg.com>
2015-05-25 13:43:22 +0200

    rm comments

--- apps/gdrawhistory/daemon.sh
+++ apps/gdrawhistory/daemon.sh
@@ -10,4 +10,3 @@ bash getgdraw.sh "dcfvg" "../../content/draw-history/" 1000 \
 			"https://docs.google.com/drawings/d/1EdyCe0d2GIS_ZrYOzA8P0gOO9Yv3xLwpEgZPR4peHGE"
 
 rsync --update -raz ~/Scripts/custom/live-cli/content/draw-history/main/*.gif -e ssh $server:~/www/live.dcfvg.com/content/7-teletext/ 
-# rsync --update -raz ~/Scripts/custom/live-cli/content/draw-history/main/*.gif -e ssh root@dcfvg.com:/root/www/live.dcfvg.com/content/7-teletext/ 
